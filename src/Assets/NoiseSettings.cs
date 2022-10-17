e466ebd22a39073fa2a7aacfac5c5d91f749d72d
milena.dionne <milena.dionne@olympus.com>
2022-10-17 03:03:51 -0400

    add background

--- src/Assets/NoiseSettings.cs
+++ src/Assets/NoiseSettings.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 [System.Serializable]
 public class NoiseSettings {
 
-    public float strength = 2;
+    public float strength = 1;
     [Range(1,8)]
-    public int numLayers = 4;
+    public int numLayers = 1;
     public float baseRoughness = 1;
     public float roughness = 2;
     public float persistence = .5f;
